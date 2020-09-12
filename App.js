474f7c981f49de252b31b866de7e442141dbe551
Mohammed <mohammed.khan3@mavs.uta.edu>
2020-09-12 16:49:09 -0500

    Login Screen and Signup Screen
    Firebase compatible. Signup and Login screen functional.

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Platform, Image, Text, View } from "react-native";
+import {
+	createSwitchNavigator,
+	createStackNavigator,
+	createAppContainer
+} from "@react-navigation/stack";
+
+import * as firebase from "firebase";
+var firebaseConfig = {
+	apiKey: "AIzaSyBLJ-dDm-yr9YouJny1tf7F5Z4CTgrtgsA",
+	authDomain: "vroomwash-f6735.firebaseapp.com",
+	databaseURL: "https://vroomwash-f6735.firebaseio.com",
+	projectId: "vroomwash-f6735",
+	storageBucket: "vroomwash-f6735.appspot.com",
+	messagingSenderId: "465276854623",
+	appId: "1:465276854623:web:8f9568eda142f82da996ea"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// import the different screens
+//import Loading from "./components/Loading";
+import Signup from "./Screens/Signup";
+import Login from "./Screens/Login";
+import Home from "./Screens/Home";
+import {NavigationContainer} from "@react-navigation/native";
+// import Otp from "./components/Phone/Otp";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Login"
+					component={Login}
+					options={{title: 'Login'}}
+					backgroundColor = '#003f5c'
+				/>
+				<Stack.Screen
+					name="Signup"
+					component={Signup}
+					options={{title: 'Signup'}}
+					backgroundColor = '#003f5c'
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+		);
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
