7b765bc5b5ba21073601fbf3590b00b1b9975ad6
Esau Noya <esaunoya@outlook.com>
2020-10-01 21:24:12 -0500

    Fix Indentation

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { StyleSheet, Platform, Image, Text, View } from "react-native";
 import {
-  createSwitchNavigator,
-  createStackNavigator,
-  createAppContainer,
+    createSwitchNavigator,
+    createStackNavigator,
+    createAppContainer,
 } from "@react-navigation/stack";
 
 import * as firebase from "firebase";
 var firebaseConfig = {
-  apiKey: "AIzaSyBLJ-dDm-yr9YouJny1tf7F5Z4CTgrtgsA",
-  authDomain: "vroomwash-f6735.firebaseapp.com",
-  databaseURL: "https://vroomwash-f6735.firebaseio.com",
-  projectId: "vroomwash-f6735",
-  storageBucket: "vroomwash-f6735.appspot.com",
-  messagingSenderId: "465276854623",
-  appId: "1:465276854623:web:8f9568eda142f82da996ea",
+    apiKey: "AIzaSyBLJ-dDm-yr9YouJny1tf7F5Z4CTgrtgsA",
+    authDomain: "vroomwash-f6735.firebaseapp.com",
+    databaseURL: "https://vroomwash-f6735.firebaseio.com",
+    projectId: "vroomwash-f6735",
+    storageBucket: "vroomwash-f6735.appspot.com",
+    messagingSenderId: "465276854623",
+    appId: "1:465276854623:web:8f9568eda142f82da996ea",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -42,90 +42,90 @@ const ServiceStack = createStackNavigator();
 const SignupStack = createStackNavigator();
 
 const HomeStackScreen = ({ navigation }) => (
-  <HomeStack.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: "#1999e3",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        fontWeight: "bold",
-      },
-    }}
-  >
-    <HomeStack.Screen
-      name="Home"
-      component={Home}
-      options={{
-        title: "Home",
-        headerLeft: () => (
-          <Icon.Button
-            name="ios-menu"
-            size={25}
-            backgroundColor={"#1999e3"}
-            onPress={() => navigation.openDrawer()}
-          ></Icon.Button>
-        ),
-      }}
-      backgroundColor="teal"
-    />
-  </HomeStack.Navigator>
+    <HomeStack.Navigator
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: "#1999e3",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                fontWeight: "bold",
+            },
+        }}
+    >
+        <HomeStack.Screen
+            name="Home"
+            component={Home}
+            options={{
+                title: "Home",
+                headerLeft: () => (
+                    <Icon.Button
+                        name="ios-menu"
+                        size={25}
+                        backgroundColor={"#1999e3"}
+                        onPress={() => navigation.openDrawer()}
+                    ></Icon.Button>
+                ),
+            }}
+            backgroundColor="teal"
+        />
+    </HomeStack.Navigator>
 );
 
 const LoginStackScreen = ({ navigation }) => (
-  <LoginStack.Navigator>
-    <LoginStack.Screen
-      name="Login"
-      component={Login}
-      options={{ title: "Login" }}
-      backgroundColor="#003f5c"
-    />
-  </LoginStack.Navigator>
+    <LoginStack.Navigator>
+        <LoginStack.Screen
+            name="Login"
+            component={Login}
+            options={{ title: "Login" }}
+            backgroundColor="#003f5c"
+        />
+    </LoginStack.Navigator>
 );
 
 const ProfileStackScreen = ({ navigation }) => (
-  <ProfileStack.Navigator>
-    <ProfileStack.Screen
-      name="Profile"
-      component={Profile}
-      options={{ title: "Profile" }}
-      backgroundColor="#003f5c"
-    />
-  </ProfileStack.Navigator>
+    <ProfileStack.Navigator>
+        <ProfileStack.Screen
+            name="Profile"
+            component={Profile}
+            options={{ title: "Profile" }}
+            backgroundColor="#003f5c"
+        />
+    </ProfileStack.Navigator>
 );
 
 const ServiceStackScreen = ({ navigation }) => (
-  <ServiceStack.Navigator>
-    <ServiceStack.Screen
-      name="Service"
-      component={Service}
-      options={{ title: "Service" }}
-      backgroundColor="#003f5c"
-    />
-  </ServiceStack.Navigator>
+    <ServiceStack.Navigator>
+        <ServiceStack.Screen
+            name="Service"
+            component={Service}
+            options={{ title: "Service" }}
+            backgroundColor="#003f5c"
+        />
+    </ServiceStack.Navigator>
 );
 
 const SignupStackScreen = ({ navigation }) => (
-  <SignupStack.Navigator>
-    <SignupStack.Screen
-      name="Signup"
-      component={Signup}
-      options={{ title: "Signup" }}
-      backgroundColor="#003f5c"
-    />
-  </SignupStack.Navigator>
+    <SignupStack.Navigator>
+        <SignupStack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ title: "Signup" }}
+            backgroundColor="#003f5c"
+        />
+    </SignupStack.Navigator>
 );
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        {/* <Drawer.Screen name="Home" component={HomeStackScreen} /> */}
-        <Drawer.Screen name="Signup" component={SignupStackScreen} />
-        <Drawer.Screen name="Login" component={LoginStackScreen} />
-        <Drawer.Screen name="Profile" component={ProfileStackScreen} />
-        <Drawer.Screen name="Service" component={ServiceStackScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+                {/* <Drawer.Screen name="Home" component={HomeStackScreen} /> */}
+                <Drawer.Screen name="Signup" component={SignupStackScreen} />
+                <Drawer.Screen name="Login" component={LoginStackScreen} />
+                <Drawer.Screen name="Profile" component={ProfileStackScreen} />
+                <Drawer.Screen name="Service" component={ServiceStackScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
--- Screens/Home.js
+++ Screens/Home.js
@@ -5,58 +5,58 @@ import * as Location from "expo-location";
 import * as Constants from "expo";
 
 export default class Home extends React.Component {
-  _isMounted = false;
-  state = {
-    location: null,
-    errorMessage: null,
-  };
-
-  componentDidMount() {
-    this._isMounted = false;
-  }
-
-  componentWillUnmount() {
-    this.isMounted = true;
-    this._getLocation().then((data) => {
-      if (this.isMounted) this.setState(data);
-    });
-    return () => {
-      this.isMounted = false;
+    _isMounted = false;
+    state = {
+        location: null,
+        errorMessage: null,
     };
-  }
 
-  // get device permissions to get device location
-  // save location
-  _getLocation = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-
-    if (status !== "granted") {
-      this.setState({
-        errorMessage: "Permission Not Granted",
-      });
+    componentDidMount() {
+        this._isMounted = false;
     }
 
-    let location = await Location.getCurrentPositionAsync({});
+    componentWillUnmount() {
+        this.isMounted = true;
+        this._getLocation().then((data) => {
+            if (this.isMounted) this.setState(data);
+        });
+        return () => {
+            this.isMounted = false;
+        };
+    }
 
-    this.setState({
-      location,
-    });
-  };
+    // get device permissions to get device location
+    // save location
+    _getLocation = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>{JSON.stringify(this.state.location)}</Text>
-      </View>
-    );
-  }
+        if (status !== "granted") {
+            this.setState({
+                errorMessage: "Permission Not Granted",
+            });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+
+        this.setState({
+            location,
+        });
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>{JSON.stringify(this.state.location)}</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "slategray",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "slategray",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
