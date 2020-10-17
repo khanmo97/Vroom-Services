b906fa92c3b151548f682fd86d55a7bd3e1089e2
esaunoya@outlook.com <esaunoya@outlook.com>
2020-10-17 01:21:20 -0500

    Get business geopoint on registration

--- Screens/Signup.js
+++ Screens/Signup.js
@@ -6,12 +6,16 @@ import { AsyncStorage } from "react-native";
 import { CheckBox } from "react-native-elements";
 import { Checkbox, Switch } from "react-native-paper";
 
+import * as Permissions from "expo-permissions";
+import * as Location from "expo-location";
+
 export default class SignUp extends React.Component {
     state = {
         email: "",
         password: "",
         isBusiness: false,
         businessName: "",
+        location: null,
         errorMessage: null,
     };
 
@@ -25,13 +29,22 @@ export default class SignUp extends React.Component {
         //     console.log("We are authenticated now!");
         //   }
         // });
+
+        // Sign out in case user tries to sign up while signed in
+        firebase.auth().signOut();
+
+        // Get users location
+        this.getLocation();
     }
 
     handleSignUp = () => {
-        // Check if business switch is toggled and no business name is given.
+        // Check if business switch is toggled and no business name/location is given.
         // Return error if that is the case
-        if (this.state.isBusiness == true && this.state.businessName === "") {
-            this.setState({ errorMessage: "Business name missing" });
+        if (
+            this.state.isBusiness == true &&
+            (this.state.businessName === "" || this.state.location == null)
+        ) {
+            this.setState({ errorMessage: "Business information missing" });
         } else {
             // create a user with email and password input
             firebase
@@ -40,28 +53,71 @@ export default class SignUp extends React.Component {
                     this.state.email,
                     this.state.password
                 )
-                .then(() => this.props.navigation.navigate("Login"))
+                .then(() => {
+                    // If registering as a business add to business document in firestore database
+                    if (this.state.isBusiness) {
+                        firebase.firestore().collection("businesses").add({
+                            UID: firebase.auth().currentUser.uid,
+                            BusinessName: this.state.businessName,
+                            Location: this.state.location,
+                        });
+                    }
+                })
+                .then(() => {
+                    this.props.navigation.navigate("Profile");
+                })
                 .catch((error) =>
                     this.setState({ errorMessage: error.message })
                 );
-            // If registering as a business add to firestore database
-            if (this.state.isBusiness && this.state.BusinessName != "") {
-                firebase.firestore().collection("businesses").add({
-                    UID: firebase.auth().currentUser.uid,
-                    BusinessName: this.state.businessName,
-                });
-            }
         }
     };
+
+    getLocation = async () => {
+        // Location permissions on device
+        const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+        if (status !== "granted") {
+            this.setState({
+                errorMessage: "Location Permissions not granted",
+            });
+        } else {
+            // Save location as firestore GeoPoint [latitude, longitude]
+            const currentPosition = await Location.getCurrentPositionAsync();
+
+            const location = new firebase.firestore.GeoPoint(
+                currentPosition.coords.latitude,
+                currentPosition.coords.longitude
+            );
+
+            this.setState({
+                location,
+            });
+        }
+    };
+
     render() {
-        //define text box for business name input for conditional rendering
-        const businessBox = (
+        //define input fields for businesses for conditional rendering
+        const locString = JSON.stringify(this.state.location);
+        const businessNameTextInput = (
             <TextInput
                 placeholder="Business Name"
                 autoCapitalize="none"
                 style={styles.textInput}
                 onChangeText={(businessName) => this.setState({ businessName })}
                 value={this.state.businessName}
+            />
+        );
+        const businessAddressTextInput = (
+            // TO DO
+            // Currently location is automatically retrieved on component mount
+            // Only able to take in object with latitude and longitude as location
+            // Future work: enable ability to enter an address and convert to lat, long
+            //
+            // Also allow business to define range willing to travel from location
+            <TextInput
+                placeholder={locString}
+                autoCapitalize="none"
+                style={styles.textInput}
             />
         );
         return (
@@ -88,12 +144,7 @@ export default class SignUp extends React.Component {
                     value={this.state.password}
                 />
 
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                    }}
-                >
+                <View style={styles.row}>
                     <Switch
                         onValueChange={(isBusiness) =>
                             this.setState({ isBusiness })
@@ -102,7 +153,10 @@ export default class SignUp extends React.Component {
                     />
                     <Text>Toggle if registering as a business</Text>
                 </View>
-                {this.state.isBusiness ? businessBox : null}
+
+                {this.state.isBusiness ? businessNameTextInput : null}
+                {this.state.isBusiness ? businessAddressTextInput : null}
+
                 <Button title="Sign Up" onPress={this.handleSignUp} />
                 <Button
                     title="Already have an account? Login"
@@ -128,5 +182,13 @@ const styles = StyleSheet.create({
         borderColor: "gray",
         borderWidth: 1,
         marginTop: 8,
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: 8,
+        height: 40,
+        width: "90%",
     },
 });
